e047331a758d3ee4a9d8d24f8cb13071390a73e8
chinweokwu <paulcmorah@gmail.com>
2022-03-30 04:20:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello world i am in react
+      </header>
+    </div>
+  );
+}
+
+export default App;
