b75f35a9fc9ca8e61e2d452b24bdbf444f742210
chinweokwu <paulcmorah@gmail.com>
2022-04-05 15:57:18 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Sessions from "./components/authentication/registrations/signup";
+import Registrations from "./components/authentication/sessions/signin";
+import NavBar from "./components/Navigation/navbar";
+
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world i am in react
-      </header>
+      <NavBar />
+      <Routes>
+        
+            {/* <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} /> 
+            <Route path="contact" element={<Contact />} />
+            <Route path="properties" element={<Properties />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="decoration" element={<Decoration />} /> */}
+            <Route path="signin" element={<Sessions />} />
+            <Route path="signup" element={<Registrations />} />
+        
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navigation/navbar.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+
+const Navigation = () => {
+  return (
+    <>
+      <CssBaseline/>
+      <AppBar>
+        11234567890
+      </AppBar>
+    </>
+  )
+}
+
+export default Navigation;
